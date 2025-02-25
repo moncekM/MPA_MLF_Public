1555e6978c40c8e882641ffe0551b98b19e22b98
Martin_Moncek <martinmonc@gmail.com>
2025-02-25 19:53:48 +0100

    Added a fix for last week PC lab and also adding a ipynb for google colab which will be shortly populuzed from working experience

--- LAB_2/Exercise1.py
+++ LAB_2/Exercise1.py
@@ -11,8 +11,8 @@ print(iris.data[0:5,:])
 print(iris.target[0:5])
 #print(iris.data)
 
-X=iris.data
-y=iris.target
+X=iris.data[iris.target!=2,0:2]
+y=iris.target[iris.target!=2]
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 print(X_train.shape)
 print(X_test.shape)
